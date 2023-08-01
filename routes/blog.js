9256393d28cb7e22db4b6f3421295143a4227a2a
Stephen Axtell <axtell.stephen@gmail.com>
2023-08-01 18:06:21 +0200

    allowing to view single post details. as well as fixing date output

--- routes/blog.js
+++ routes/blog.js
@@ -51,4 +51,28 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { projection: { summary: 0 } });
+  // 0 in project means ignore and use the rest
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  post.date = post.date.toISOString();
+
+  console.log(post.date);
+  res.render("post-detail", { post: post });
+});
+
 module.exports = router;
